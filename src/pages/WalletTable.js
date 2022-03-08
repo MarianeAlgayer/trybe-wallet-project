59d5b5a0d79b1f7371e25e63eb7d18deba815cf6
Mariane Algayer <marianealgayer@gmail.com>
2022-03-08 13:43:38 -0300

    refactor: format wallet table numbers

--- src/pages/WalletTable.js
+++ src/pages/WalletTable.js
@@ -8,35 +8,44 @@ class WalletTable extends React.Component {
 
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-        { expenses.map(({
-          id, description, tag, method, value, currency, exchangeRates,
-        }) => (
-          <tr key={ id }>
-            <td>{ description }</td>
-            <td>{ tag }</td>
-            <td>{ method }</td>
-            <td>{ value }</td>
-            <td>{ exchangeRates[currency].name }</td>
-            <td>{ exchangeRates[currency].ask }</td>
-            <td>{ value * exchangeRates[currency].ask }</td>
-            <td>Real</td>
-            <td>
-              <button type="button">Editar</button>
-              <button type="button">Excluir</button>
-            </td>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          { expenses.map(({
+            id, description, tag, method, value, currency, exchangeRates,
+          }) => {
+            const currencyName = exchangeRates[currency].name;
+            const exchangeRate = Number(exchangeRates[currency].ask).toFixed(2);
+            const convertedValue = Number(value * exchangeRates[currency].ask).toFixed(2);
+
+            return (
+              <tr key={ id }>
+                <td>{ description }</td>
+                <td>{ tag }</td>
+                <td>{ method }</td>
+                <td>{ Number(value).toFixed(2) }</td>
+                <td>{ currencyName }</td>
+                <td>{ exchangeRate }</td>
+                <td>{ convertedValue }</td>
+                <td>Real</td>
+                <td>
+                  <button type="button">Editar</button>
+                  <button type="button">Excluir</button>
+                </td>
+              </tr>);
+          })}
+        </tbody>
       </table>
     );
   }
