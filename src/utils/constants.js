bb78bac29c821db178ae6fa5d28b34341346d2f2
Mariane Algayer <marianealgayer@gmail.com>
2022-06-18 22:32:37 -0300

    feat: constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const HEADERS = [
+  'Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda', 'Câmbio utilizado',
+  'Valor convertido', 'Moeda de conversão', 'Excluir',
+];
+
+export const CATEGORIES_OPTIONS = [
+  'Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde',
+];
+
+export const PAYMENT_OPTIONS = [
+  'Dinheiro', 'Cartão de crédito', 'Cartão de débito',
+];
