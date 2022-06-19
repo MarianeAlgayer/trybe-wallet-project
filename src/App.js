95eec271df9fea0a77ab0e6ac568b6a53cd06818
Mariane Algayer <marianealgayer@gmail.com>
2022-06-18 22:50:37 -0300

    refactor: delete index.css and create main.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
+
+import './styles/main.css';
 
 function App() {
   return (
