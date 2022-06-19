65542632244eac3dbc0706251074cb69b4fd53c5
Mariane Algayer <marianealgayer@gmail.com>
2022-06-18 22:58:29 -0300

    Updates

---
+++ precache-manifest.13066cae867021c21e1d54331454c515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc36c9aa0d5214a7f3798eb33f1f0396",
+    "url": "/trybe-wallet-project/index.html"
+  },
+  {
+    "revision": "3eeba97a2fa53e89a8ec",
+    "url": "/trybe-wallet-project/static/css/main.5615c65b.chunk.css"
+  },
+  {
+    "revision": "08e0fd26c248e86d0186",
+    "url": "/trybe-wallet-project/static/js/2.1caacc51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet-project/static/js/2.1caacc51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eeba97a2fa53e89a8ec",
+    "url": "/trybe-wallet-project/static/js/main.54b521dc.chunk.js"
+  },
+  {
+    "revision": "6cb54844c81d22b4077d",
+    "url": "/trybe-wallet-project/static/js/runtime-main.515eb8bd.js"
+  }
+]);
